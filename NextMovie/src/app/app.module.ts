6a352bd3c0ea3f96e5f40d608ac86e5826975fbb
Matimed <mcarazomedley@gmail.com>
2022-11-21 19:19:56 -0300

    File reordering

--- NextMovie/src/app/app.module.ts
+++ NextMovie/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { SimilarComponent } from './components/similar/similar/similar.component';
-import { MovieComponent } from './components/movie/movie/movie.component';
+import { SimilarComponent } from './components/similar/similar.component';
+import { MovieComponent } from './components/movie/movie.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { LikedComponent } from './components/filter/liked/liked.component';
-import { WhichComponent } from './components/filter/which/which/which.component';
+import { WhichComponent } from './components/filter/which/which.component';
 
 @NgModule({
   declarations: [
--- NextMovie/src/app/components/filter/filter.component.ts
+++ NextMovie/src/app/components/filter/filter.component.ts
@@ -3,13 +3,5 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
 })
-export class FilterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FilterComponent { }
